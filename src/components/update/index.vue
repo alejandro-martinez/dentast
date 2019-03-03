dc06c4821ae3a6dbab6bc5f2b6b358ef94ae5492
Alejandro <alejandromartinezap@gmail.com>
2019-03-03 14:53:20 -0300

    Last changes

--- src/components/update/index.vue
+++ src/components/update/index.vue
@@ -5,7 +5,9 @@
         <div class="row align-items-center">
           <div class="col-12">
             <i class="fa fa-align-justify"></i>
+            <h4> Version actual: {{ version }} </h4>
             <h4 v-show="updating">Actualizando sistema, espere por favor ...</h4>
+            <h4 v-show="!updated && !updating">No hay actualizaciones disponibles...</h4>
             <h4 v-if="!updating && updated">El sistema ha sido actualizado a la version: {{ version }}. Presione f5 para completar la operacion.</h4>
           </div>
           <div class="col-12">
