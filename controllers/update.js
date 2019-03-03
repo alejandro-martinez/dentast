b0946325f69e9a8acc4e69827170d378a60a30fc
Alejandro <alejandromartinezap@gmail.com>
2019-03-03 12:49:44 -0300

    Added model

--- controllers/update.js
+++ controllers/update.js
@@ -1,3 +1,5 @@
+const System = require('../models/system.js');
+
 const USER = 'alejandro-martinez';
 const PASS = '55a5f40a09568af5f3e464f616b082a534d7a0be';
 const REPO = 'github.com/alejandro-martinez/dentast.git';
@@ -10,13 +12,22 @@ const remoteUrl = `https://${USER}:${PASS}@${REPO}`;
 module.exports = (router) => {
 
   router.get('/version', (req, res, next) => {
-  	var pjson = require('../package.json');
-  	git().fetch(remoteUrl, 'master').then((response) => {
+  	let pjson = require('../package.json');
+    const currentVersion = pjson.version;
+  	git().pull(remoteUrl, 'master').then((response) => {
   		if (response) {
-  			console.log(response);
+        const insertions = _.get(response, 'summary.insertions', 0) > 0;
+        const deletions = _.get(response, 'summary.deletions', 0) > 0;
+        const changes = _.get(response, 'summary.changes', 0) > 0;
+  			const shouldUpdate = insertions || deletions || changes;
+        const newVersion = require('../package.json');
+        // Mark system as it should be updated
+        Patient.findOneAndUpdate({ version: currentVersion }, {
+          outdated: true,
+        });
   			res.json({
 	  			currentVersion: pjson.version,
-	  			remoteVersion: response,
+	  			remoteVersion: newVersion,
 	  		});	
   		}
   	}).catch(() => {
