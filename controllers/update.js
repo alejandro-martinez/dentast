78c5f691edaa182cd96d5ebe12b9e810627dc69e
Alejandro Martinez <alejandromartinezap@gmail.com>
2019-03-11 15:10:48 -0300

    Always update

--- controllers/update.js
+++ controllers/update.js
@@ -10,28 +10,21 @@ const pjson = require('../package.json');
 module.exports = (router) => {
   router.post('/update', (req, res) => {
     const version = pjson.version;
-    git().pull(remoteUrl, 'master').then((response) => {
-      if (response && response.summary.changes > 1) {
-        console.log('about to update');
-        return exec('npm run-script build', (err) => {
-          if (err) {
-            return res.status(500).json({ err });
-          }
-          return fs.readFile('./package.json', (pjson2) => {
-            console.log(pjson2);
-            const packageJson = JSON.parse(pjson2);
-            res.status(200).json({
-              updated: true,
-              version: packageJson.version,
-            });
+    git().pull(remoteUrl, 'master').then(() =>
+      exec('npm run-script build', (err) => {
+        if (err) {
+          return res.status(500).json({ err });
+        }
+        return fs.readFile('./package.json', (pjson2) => {
+          console.log(pjson2);
+          const packageJson = JSON.parse(pjson2);
+          res.status(200).json({
+            updated: true,
+            version: packageJson.version,
           });
         });
-      }
-      return res.status(200).json({
-        updated: false,
-        version,
-      });
-    }).catch(() => {
+      }),
+    ).catch(() => {
       res.status(200).json({
         version,
         updated: false,
